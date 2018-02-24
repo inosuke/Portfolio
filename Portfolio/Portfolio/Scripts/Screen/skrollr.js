e8a1fc903b95d9daea03055b296c10b8e1325f81
yamasaki.yuuki <yamasaki.yuuki@toyoko-sys.co.jp>
2018-02-24 15:09:13 +0900

    仮追加

--- Portfolio/Portfolio/Scripts/Screen/skrollr.js
+++ Portfolio/Portfolio/Scripts/Screen/skrollr.js
@@ -4,8 +4,4 @@
 //        $("#scroll").text("スクロール量：" + y); //追加
 //    });
 ////});
-//$(window).scroll(function () { //スクロールイベント
-//    var y = $(this).scrollTop();
-//        //$("#scroll").text("スクロール量：" + y); //追加
-//});
 //# sourceMappingURL=skrollr.js.map
